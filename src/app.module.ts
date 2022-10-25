cc6e20d5431bba750dfbbb5db1bcc1b95c26764b
Anna Boval <anna.bo1106@gmail.com>
2022-10-25 11:37:52 +0200

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PizzasModule } from './pizzas/pizzas.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    PizzasModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
